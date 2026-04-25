a04d21499df1af0576f54e02980d527b6b70c01e
jaehyeon2k <wmspfl2673@gmail.com>
2026-04-25 23:15:40 +0900

    fix: API 포트 번호 수정 및 협업용 백엔드 코드 주석 추가

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 공통 API 설정 (백엔드 서버 주소)
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
