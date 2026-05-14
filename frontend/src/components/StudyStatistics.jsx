49b9033a1787deb5535eaaaa1f72248f9b1783f7
jaehyeon2k <wmspfl2673@gmail.com>
2026-05-14 21:09:00 +0900

    Feat: 백엔드 상호 피드백/트랜잭션 복구

--- frontend/src/components/StudyStatistics.jsx
+++ frontend/src/components/StudyStatistics.jsx
@@ -86,7 +86,7 @@ export default function StudyStatistics({ todayStudySeconds = 0 }) {
       console.log("StudyStatistics loadData weeklyResult:", weeklyResult);
       let rawData = Array.isArray(weeklyResult)
         ? weeklyResult
-        : (weeklyResult?.dailyStats || []);
+        : (weeklyResult?.data || weeklyResult?.dailyStats || []);
 
       setBaseRawData(rawData);
 
