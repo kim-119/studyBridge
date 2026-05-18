34f2e90e983c4bd8b6706dcbd98d3acf8af7962f
jaehyeon2k <wmspfl2673@gmail.com>
2026-05-18 18:12:20 +0900

    배포하기 위한 CORS 정책 변경

--- backend/src/main/java/com/studybridge/api/config/SecurityConfig.java
+++ backend/src/main/java/com/studybridge/api/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
